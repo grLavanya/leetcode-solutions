975181c7eab1b41b7aad548032cee488e3872692
Lavanya G R <grlavanya0907@gmail.com>
2026-05-01 18:31:53 +0530

    Added Two Sum solution

---
+++ 001_two_sum.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        h = {}
+
+        for i in range(len(nums)):
+
+            h[nums[i]] = i
+
+	 
+
+        for i in range(len(nums)):
+
+            y = target - nums[i]
+
+	 
+
+            if y in h and h[y] != i:
+
+                return [i, h[y]]
+
+# Time Complexity: O(n)
+
+# Space Complexity: O(n)
+
+# Two-pass Hash Table
+
+# The time complexity is O(n) because we traverse the list containing n elements exactly twice. Since the hash table reduces the look-up time to O(1), the overall time complexity is O(n).
+
+# The space complexity is O(n) because in the worst case, we could be storing all n elements in the hash table. Therefore, the space required grows linearly with the size of the input list.
