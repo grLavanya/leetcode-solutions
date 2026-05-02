94014a85535a64d3e4183ad1b30f09f704f196c3
Lavanya G R <grlavanya0907@gmail.com>
2026-05-02 17:49:33 +0530

    Optimized using character frequency mapping

--- 003_valid_anagram.py
+++ 003_valid_anagram.py
@@ -1,5 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-       if "".join(sorted(s))=="".join(sorted(t)):
+        freq_s = {}
+        freq_t = {}
+        for char in s:
+            freq_s[char] = freq_s.get(char, 0) + 1
+        for char in t:
+            freq_t[char] = freq_t.get(char, 0) + 1
+        if freq_s == freq_t:
             return True
-       return False
+        return False
