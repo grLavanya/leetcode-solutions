9b017da7b2cd948f0f70f56e77dc03ec0206bcfb
Lavanya G R <grlavanya0907@gmail.com>
2026-05-02 18:02:32 +0530

    Tried to narrow down by immediately eliminating strings of different lengths

--- 003_valid_anagram.py
+++ 003_valid_anagram.py
@@ -1,11 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        freq_s = {}
-        freq_t = {}
-        for char in s:
-            freq_s[char] = freq_s.get(char, 0) + 1
-        for char in t:
-            freq_t[char] = freq_t.get(char, 0) + 1
-        if freq_s == freq_t:
+        if len(s) != len(t):
+            return False
+        else:
+            if sorted(s) != sorted(t):
+                return False
             return True
-        return False
