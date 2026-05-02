bcc92e55e112af7b82e280604db8d773af78df57
Lavanya G R <grlavanya0907@gmail.com>
2026-05-02 17:45:23 +0530

    Shortened the code using direct sorting

--- 003_valid_anagram.py
+++ 003_valid_anagram.py
@@ -1,7 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        sorted_s="".join(sorted(s))
-        sorted_t="".join(sorted(t))
-        if sorted_s==sorted_t:
+       if "".join(sorted(s))=="".join(sorted(t)):
             return True
-        return False
+       return False
